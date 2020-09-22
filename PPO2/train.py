b679b8e3a75f1348d125524bdebeb9429609404f
imb19jan9 <imb19jan9@gmail.com>
2020-09-22 17:20:41 +0900

    exp_name modified

--- PPO2/train.py
+++ PPO2/train.py
@@ -1,16 +1,16 @@
 from ppo import ppo
-from env import SupportEnv, ImageToPyTorch, ScaledFloatFrame
+from env import SupportEnv_v0, ImageToPyTorch, ScaledFloatFrame
 from model import ResActorCritic
 
 from spinup.utils.run_utils import setup_logger_kwargs
 
 seed = 0
-env_fn = lambda: ScaledFloatFrame(ImageToPyTorch(SupportEnv()))
+env_fn = lambda: ScaledFloatFrame(ImageToPyTorch(SupportEnv_v0()))
 
-ac_kwargs = dict(n_channel=64, n_block=6)
+ac_kwargs = dict(n_channel=128, n_block=6)
 
 logger_kwargs = dict(output_dir="./data", exp_name="exp1")
-logger_kwargs = setup_logger_kwargs(exp_name="8x8", seed=seed, data_dir="./data", datestamp=True)
+logger_kwargs = setup_logger_kwargs(exp_name="board8_nc128_nb6", seed=seed, data_dir="./data", datestamp=True)
 ppo(
     env_fn=env_fn,
     actor_critic=ResActorCritic,
