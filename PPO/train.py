a22deb4d6dd4f921f5c3a3df40a1f89c33c05cc6
imb19jan9 <imb19jan9@gmail.com>
2020-09-24 18:05:16 +0900

    linear schedule, saving callback added

--- PPO/train.py
+++ PPO/train.py
@@ -1,9 +1,31 @@
 from stable_baselines3 import PPO
 from stable_baselines3.common.cmd_util import make_vec_env
+from stable_baselines3.common.callbacks import CheckpointCallback
 
 from env import SupportEnv_v1, ImageToPyTorch, ScaledFloatFrame
 from policy import MyActorCriticPolicy
 from model import ResFeatureExtractor
+
+
+def linear_schedule(initial_value):
+    """
+    Linear learning rate schedule.
+    :param initial_value: (float or str)
+    :return: (function)
+    """
+    if isinstance(initial_value, str):
+        initial_value = float(initial_value)
+
+    def func(progress):
+        """
+        Progress will decrease from 1 (beginning) to 0
+        :param progress: (float)
+        :return: (float)
+        """
+        return progress * initial_value
+
+    return func
+
 
 if __name__ == "__main__":
     seed = 0
@@ -29,11 +51,11 @@ if __name__ == "__main__":
     model = PPO(
         MyActorCriticPolicy,
         env,
-        learning_rate=3e-4,
-        n_steps=1024,
+        learning_rate=linear_schedule(3e-4),
+        n_steps=2048,
         batch_size=64,
         n_epochs=10,
-        gamma=0.99,
+        gamma=0.95,
         gae_lambda=0.95,
         clip_range=0.2,
         clip_range_vf=0.2,
@@ -41,8 +63,7 @@ if __name__ == "__main__":
         vf_coef=0.5,
         max_grad_norm=0.5,
         target_kl=0.05,
-        # tensorboard_log=f"runs/v1_board8_nc{n_channel}_nb{n_block}_seed{seed}",
-        tensorboard_log=f"runs/v1_board9_natruecnn_seed{seed}",
+        tensorboard_log=f"runs/v1_board8_nc{n_channel}_nb{n_block}_seed{seed}",
         policy_kwargs=policy_kwargs,
         verbose=1,
         seed=seed,
@@ -50,6 +71,10 @@ if __name__ == "__main__":
 
     print(model.policy)
 
-    for i in range(100):
-        model.learn(total_timesteps=1e6)
-        model.save(f"ppo_model_{i}")
+    checkpoint_callback = CheckpointCallback(
+        save_freq=1e6, save_path="./logs/", name_prefix="rl_model"
+    )
+
+    model.learn(
+        total_timesteps=1e7, reset_num_timesteps=False, callback=checkpoint_callback
+    )
